c48d7fc5a8d8f5af5a35268a57582aeb47658059
bvo24 <brianvo2020@gmail.com>
2024-04-28 13:42:16 -0400

    Day 26 -Implementation for hours and minutes -Limit on date ranges

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var sleepAmount = 8.0
+    @State private var wakeUp = Date.now
     
     
     var body: some View {
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in:4...12, step: 0.5)
+        //Displaying only hours and minutes
+        //DatePicker("Please enter a date", selection: $wakeUp, displayedComponents: .hourAndMinute)
+        //Limits on date
+        DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+            .labelsHidden()
+        
     }
+    func exampleDates(){
+        let tomorrow = Date.now.addingTimeInterval(86400)
+        let range = Date.now...tomorrow
+    }
+    
 }
 
 #Preview {
