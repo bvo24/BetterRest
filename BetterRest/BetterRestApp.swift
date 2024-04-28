9cbf7a5dbdeaceb7b9d4264182c2bd3d80594f5f
bvo24 <brianvo2020@gmail.com>
2024-04-28 13:22:26 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Brian Vo on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
