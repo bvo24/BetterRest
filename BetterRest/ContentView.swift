c38939d7cafdcbb8442e585052548e815426c1b3
bvo24 <brianvo2020@gmail.com>
2024-04-29 15:27:46 -0400

    Day 28 -Changed stepper to a picker (coffee cups) -Removed the add button and always show a time now

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -27,37 +27,57 @@ struct ContentView: View {
     var body: some View {
         NavigationStack{
             Form{
-                
                 VStack(alignment: .leading, spacing: 0){
-                    Text("When do you want to wake up?")
-                        .font(.headline)
                     
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
+                    
+                    VStack(alignment: .leading, spacing: 0){
+                        Text("When do you want to wake up?")
+                            .font(.headline)
+                        
+                        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                            .labelsHidden()
+                    }
+                    
+                    VStack(alignment: .leading, spacing: 0){
+                        Text("Desired ammount of sleep")
+                            .font(.headline)
+                        Stepper("\(sleepAmmount.formatted()) hours", value: $sleepAmmount, in: 4...12, step: 0.25 )
+                    }
+                    VStack(alignment: .leading, spacing: 0){
+                        Text("Daily coffee intake")
+                            .font(.headline)
+                        //Short cut for handling singular / plural
+                        //                    Stepper("^[\(coffeeAmmount) cup](inflect: true)", value: $coffeeAmmount, in: 0...20)
+                        Picker("Cups of cofee", selection: $coffeeAmmount){
+                            ForEach(0..<21){
+                                Text("\($0)")
+                            }
+                        }
+                    }
+                    
+                    
+                }
+                Section(header: Text("Reccomended bed time").font(.headline)){
+                    
+                    Text("\(calculateBedString())")
                 }
                 
-                VStack(alignment: .leading, spacing: 0){
-                    Text("Desired ammount of sleep")
-                        .font(.headline)
-                    Stepper("\(sleepAmmount.formatted()) hours", value: $sleepAmmount, in: 4...12, step: 0.25 )
-                }
-                VStack(alignment: .leading, spacing: 0){
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    //Short cut for handling singular / plural
-                    Stepper("^[\(coffeeAmmount) cup](inflect: true)", value: $coffeeAmmount, in: 0...20)
-                }
             }
             .navigationTitle("BetterRest")
-            .toolbar{
-                Button("Calculate", action: calculateBedTime)
-            }
+//            .toolbar{
+//                Button("Calculate", action: calculateBedTime)
+//            }
             .alert(alertTitle, isPresented: $showingAlert){
                 Button("Okie"){}
                 
             }message: {
                 Text(alertMessage)
             }
+            
+            
+            
+            
+            
         }
         
 
@@ -69,7 +89,7 @@ struct ContentView: View {
     }
     
     
-    
+    //for button
     func calculateBedTime(){
         do{
             let config = MLModelConfiguration()
@@ -96,9 +116,44 @@ struct ContentView: View {
         
     }
     
+    func calculateBedString() -> String{
+        let userMsg : String
+        do{
+            let config = MLModelConfiguration()
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60
+            let minute = ( components.minute ?? 0 ) * 60
+            
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmmount, coffee: Double(coffeeAmmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            //alertTitle = "Ideal bed time is "
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+                        
+            userMsg = formatter.string(from: sleepTime)
+            
+        }
+        catch{
+            userMsg = "Error"
+            
+            
+        }
+        return userMsg
+        
+    }
+    
+    
+    
+
+    
     
     
 }
+
+
 
 #Preview {
     ContentView()
